4fe8fbd8a34aa9a45331ad3e6d2302c23218e426
Nguyễn Minh Hạnh <85898861+letsmailhanh@users.noreply.github.com>
2023-04-27 21:45:19 +0700

    Update COVID Portfolia Project.sql

--- COVID Portfolia Project.sql
+++ COVID Portfolia Project.sql
@@ -1,10 +1,21 @@
+/*
+Covid 19 Data Exploration
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 Select * 
 from PortfolioProject..CovidDeaths
 where continent is not null
 order by 3,4
 
+
 Select * from PortfolioProject..CovidVaccinations
 order by 3,4
+
+
+-- Select Data that we are going to be starting with
 
 Select location, date, total_cases, new_cases, total_deaths, population
 from PortfolioProject..CovidDeaths
@@ -14,6 +25,7 @@ order by 1,2
 
 -- Looking at Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contracts Covid in your country
+
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from PortfolioProject..CovidDeaths
 where location like '%vietnam%'
@@ -22,29 +34,26 @@ order by 1,2
 
 
 -- Looking at Total Cases vs Population
--- Shows what percentage of population got Covid
+-- Shows what percentage of population infected with Covid
 
 Select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
 from PortfolioProject..CovidDeaths
---where location like '%vietnam%'
 order by 1,2 
 
 
--- Looking at Countries with Highest Infection Rate compared to Population
+-- Countries with Highest Infection Rate compared to Population
 
-Select location, population, Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as 
+Select location, population, date, Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as 
 	PercentPopulationInfected
 from PortfolioProject..CovidDeaths
---where location like '%vietnam%'
-Group by location, population
+Group by location, population, date
 order by PercentPopulationInfected desc
 
 
--- Showing Countries with Highest Death Count per Population
+-- Countries with Highest Death Count per Population
 
 Select location, MAX(Total_deaths) as TotalDeathCount
 from PortfolioProject..CovidDeaths
---where location like '%vietnam%'
 where continent is not null
 Group by location
 order by TotalDeathCount desc
@@ -53,12 +62,10 @@ order by TotalDeathCount desc
 
 --LET'S BREAK THINGS DOWN BY CONTINENT
 
-
 -- Showing continents with the highest death count per population
 
 Select continent, MAX(Total_deaths) as TotalDeathCount
 from PortfolioProject..CovidDeaths
---where location like '%vietnam%'
 where continent is not null
 Group by continent
 order by TotalDeathCount desc
@@ -75,10 +82,19 @@ where continent is not null
 --group by date
 order by 1,2
 
+
+
+Select location, SUM(new_deaths) as TotalDeathCount
+from PortfolioProject..CovidDeaths
+where continent is null
+and location not in ('World', 'European Union') and location not like '%income%'
+group by location
+order by TotalDeathCount desc
+
  
 
  -- Looking at Total Population vs Vaccination
- -- USE CTE
+ -- Using CTE to perform Calculation on Partition By
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as 
@@ -98,7 +114,7 @@ from PopvsVac
 
 
 
--- TEMP TABLE
+-- Using Temp Table to perform Calculation on Partition By
 
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -112,7 +128,6 @@ RollingPeopleVaccinated numeric
 )
 
 Insert into #PercentPopulationVaccinated
-
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 SUM(convert(bigint, vac.new_vaccinations)) Over (Partition by dea.location 
 order by dea.location, dea.Date) as RollingPeopleVaccinated
@@ -125,6 +140,7 @@ join PortfolioProject..CovidVaccinations vac
 
 Select *, RollingPeopleVaccinated/Population*100 as PercentPopulationVaccinated
 from #PercentPopulationVaccinated
+
 
 
 
@@ -141,9 +157,5 @@ join PortfolioProject..CovidVaccinations vac
 where dea.continent is not null
 --order by 2,3
 
-
 Select * 
 From PercentPopulationVaccinated
-
-
-
